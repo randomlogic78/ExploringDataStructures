f37d63f3d15e1acb8225c6f2344ebc5626f262da
R.J. Linton <randomlogic78@gmail.com>
2019-04-14 20:42:01 -0400

    Working on json test file for rapidjson lib

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ExploringDataStructures
 A collection of experiments with data structures
+
+# RapidJson submodule required
+don't forget to run 'git submodule init after' clone.
---
+++ jsonTest.cpp
@@ -0,0 +1,11 @@
+#include "lib/rapidjson/include/rapidjson/document.h"
+#include "lib/rapidjson/include/rapidjson/prettywriter.h"
+
+using namespace rapidjson;
+
+int main(){
+
+	//Create a json document type
+	Document document;
+
+}
